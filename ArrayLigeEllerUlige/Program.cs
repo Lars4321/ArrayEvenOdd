242757713ba4f8f564ca365b0e96842a81857021
Lars <musiklars@mail.dk>
2019-08-24 22:32:08 +0200

    init

---
+++ ArrayLigeEllerUlige/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ArrayLigeEllerUlige
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast antal tal du ønsker at lægge i dit array ( max 10): ");
+
+           int num1 = int.Parse(Console.ReadLine());
+
+           
+            int[] result = new int[num1];
+
+
+
+            for (int i = 0; i < num1;)
+            {
+                Console.WriteLine("Indtast " + (num1 - i) + " numre");
+                int number;
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    result[i] = number;
+                    i++; //only increase "i" if input is valid
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Du har tastet et bogstav, prøv igen!");
+                    continue;  // skip rest of loop
+                }
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("lige tal : ", number);
+                }
+                else
+                {
+                    Console.WriteLine("ulige tal : ", number);
+                }                
+            }
+         Console.ReadLine();
+        }
+    }
+}
