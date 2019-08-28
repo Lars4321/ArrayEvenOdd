223469f2b5b721faf1b7ddacedcc2925a971e593
Lars <musiklars@mail.dk>
2019-08-28 15:27:13 +0200

    new code from tutorialpoint
    old code commented out

--- ArrayLigeEllerUlige/Program.cs
+++ ArrayLigeEllerUlige/Program.cs
@@ -6,36 +6,66 @@ namespace ArrayLigeEllerUlige
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Indtast antal tal du ønsker at lægge i dit array ( max 10): ");
+            /* Console.WriteLine("Indtast antal tal du ønsker at lægge i dit array ( max 10): ");          
+             int num1 = int.Parse(Console.ReadLine());
+             int number;
+             int[] resultEven = new int[num1];
+             int[] resultOdd = new int[num1];
 
-           int num1 = int.Parse(Console.ReadLine());
+             for (int i = 0; i < num1; i++)
+             {
+                 Console.WriteLine("Indtast " + (num1 - i) + " numre");
 
+                     number = int.Parse(Console.ReadLine());
 
-            // erklære et array her?
-            
+                 if (number % 2 == 0)
+                 {
+                     resultEven[i] = number;
+                 }
+                 else
+                 {
+                     resultOdd[i] = number;                    
+                 }                
+             }
 
-            for (int i = 0; i < num1; i++)
+             foreach (int x in resultEven)
+             {
+                 Console.WriteLine(resultEven[num1]);
+             }
+
+             foreach (int y in resultOdd)
+             {
+                 Console.WriteLine(resultOdd[num1]);
+             }
+             Console.ReadLine();*/
+
+         Console.WriteLine("Indtast antal tal du ønsker at lægge i dit array ( max 10): ");
+         int num1 = int.Parse(Console.ReadLine());
+         int[] arr1 = new int[num1];
+         int[] resultEven = new int[num1];
+         int[] resultOdd = new int[num1];
+         int i, j = 0, k = 0;
+         for (i = 0; i < num1; i++)
             {
                 Console.WriteLine("Indtast " + (num1 - i) + " numre");
-                int number;
-                
-                    number = int.Parse(Console.ReadLine());
-                    //result[i] = number;
-                                   
-                if (number % 2 == 0)
-                {
-                 int[] resultEven = new int[number];
-                    //Console.WriteLine("lige tal : ", number);
-                }
-                else
-                {
-                    int[] resultOdd = new int[number];
-                    //Console.WriteLine("ulige tal : ", number);
-                }                
+
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0) {
+               resultEven[j] = number;
+               j++;
+            } else {
+               resultOdd[k] = number;
+               k++;
             }
-
-
-         Console.ReadLine();
+         }
+         Console.WriteLine("Even numbers...");
+         for (i = 0; i < j; i++) {
+            Console.WriteLine(resultEven[i]);
+         }
+        Console.WriteLine("Odd numbers...");
+         for (i = 0; i < k; i++) {
+            Console.WriteLine(resultOdd[i]);
+         }
         }
     }
 }
